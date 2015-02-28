a01c88508e06b783bae67a677ea642ba37e43eea
Isaac Elias <isaac@walkerandcobrands.com>
2015-02-28 03:55:33 -0800

    searching in the action bar works.

--- app/src/main/java/com/codepath/pixurch/activities/SearchActivity.java
+++ app/src/main/java/com/codepath/pixurch/activities/SearchActivity.java
@@ -1,12 +1,13 @@
 package com.codepath.pixurch.activities;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.support.v7.widget.SearchView;
 
 import com.codepath.pixurch.R;
 import com.codepath.pixurch.adapters.ResultAdapter;
@@ -34,15 +35,6 @@ public class SearchActivity extends ActionBarActivity {
         aResults = new ResultAdapter(getApplicationContext(), imageResults);
         rvResults.setAdapter(aResults);
 
-            new SearchRequest("Emma Stone").response(new SearchRequest.ResultLoaderListener() {
-                @Override
-                public void onResultLoaded(ArrayList<ImageResult> resultsArray) {
-                    imageResults.addAll(resultsArray);
-                    aResults.notifyDataSetChanged();
-
-                    Toast.makeText(getApplicationContext(), "Data loaded", Toast.LENGTH_SHORT).show();
-                }
-            });
 
 
     }
@@ -52,6 +44,27 @@ public class SearchActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_search, menu);
+        MenuItem searchBar = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView)  MenuItemCompat.getActionView(searchBar);
+        searchView.setOnQueryTextListener(new android.support.v7.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                new SearchRequest(query).response(new SearchRequest.ResultLoaderListener() {
+                    @Override
+                    public void onResultLoaded(ArrayList<ImageResult> resultsArray) {
+                        imageResults.addAll(resultsArray);
+                        aResults.notifyDataSetChanged();
+                    }
+                });
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         return true;
     }
 
